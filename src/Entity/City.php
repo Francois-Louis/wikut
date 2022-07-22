3ff2fa6d156c6bb02838289e96c9b8a10a88ae90
Francois-Louis <fl.toussaint.art@gmail.com>
2022-07-22 11:37:00 +0200

    add city entity+test

---
+++ src/Entity/City.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CityRepository::class)
+ */
+class City
+{
+
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=8)
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="decimal", precision=11, scale=8)
+     */
+    private $lng;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLat(): ?string
+    {
+        return $this->lat;
+    }
+
+    public function setLat(string $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLng(): ?string
+    {
+        return $this->lng;
+    }
+
+    public function setLng(string $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+}
---
+++ tests/CityTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\City;
+use PHPUnit\Framework\TestCase;
+
+class CityTest extends TestCase
+{
+    public function testCityTrue()
+    {
+        $city = new City();
+
+        $city->setName('City');
+        $city->setLng(125.982);
+        $city->setLat(96.3715);
+
+        $this->assertTrue($city->getName() === 'City');
+        $this->assertTrue($city->getLng() === 125.982);
+        $this->assertTrue($city->getLat() === 96.3715);
+    }
+
+    public function testCityFalse(): void
+    {
+        $city = new City();
+
+        $city->setName('City');
+        $city->setLng(255.982);
+        $city->setLat(98.3715);
+
+        $this->assertFalse($city->getName() === 'wood');
+        $this->assertFalse($city->getLng() === 125.982);
+        $this->assertFalse($city->getLat() === 96.3715);
+    }
+
+    public function testCityEmpty()
+    {
+        $city = new City();
+
+        $this->assertEmpty($city->getName());
+        $this->assertEmpty($city->getLng());
+        $this->assertEmpty($city->getLat());
+    }
+}
